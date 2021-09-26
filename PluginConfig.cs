359c8b2d4c95e1ae86ac2f626447e70db73b2a38
Roy de Jong <email@roydejong.net>
2021-09-27 01:17:55 +0200

    wip: set config defaults

--- PluginConfig.cs
+++ PluginConfig.cs
@@ -5,14 +5,14 @@
         /// <summary>
         /// If enabled, show avatar mirror in lobby.
         /// </summary>
-        public bool EnableLobbyMirror;
+        public bool EnableLobbyMirror = true;
         /// <summary>
         /// If enabled, allow self-hologram in multiplayer matches, if you are in 1st places.
         /// </summary>
-        public bool EnableSelfHologram;
+        public bool EnableSelfHologram = true;
         /// <summary>
         /// If enabled, force self-hologram in multiplayer matches, never showing other player's avatars.
         /// </summary>
-        public bool ForceSelfHologram;
+        public bool ForceSelfHologram = true;
     }
 }
